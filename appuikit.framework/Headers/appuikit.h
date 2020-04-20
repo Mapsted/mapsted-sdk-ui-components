d71b5f7381ee3e5a2efab758736bdafb95b801f4
Priyo <priyo.bhattacharya@mapsted.com>
2020-04-20 13:52:18 -0400

    Updates

---
+++ appuikit.framework/Headers/appuikit.h
@@ -0,0 +1,20 @@
+//
+//  appuikit.h
+//  appuikit
+//
+//  Created by Mapsted on 2020-04-20.
+//  Copyright © 2020 Mapsted. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for appuikit.
+FOUNDATION_EXPORT double appuikitVersionNumber;
+
+//! Project version string for appuikit.
+FOUNDATION_EXPORT const unsigned char appuikitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <appuikit/PublicHeader.h>
+
+
+
